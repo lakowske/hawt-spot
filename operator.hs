385fe35c17f0b68bd848dce39ae23bf80c8f7d00
Seth Lakowske <lakowske@gmail.com>
2020-09-20 22:48:03 -0500

    add string interpolate

--- operator.hs
+++ operator.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+import Data.String.Interpolate ( i )
 import System.Process
 import System.Environment
 import System.Exit
 
 data Shell a = Shell a deriving (Show)
+
 
 
 instance Functor Shell where
@@ -17,6 +21,9 @@ instance Monad Shell where
   Shell x >>= f = f x
 
   
+appdir = "~/.hawt-spot"
+globaluser = "pi"
+globalhost = "192.168.254.14"
 
 toProc :: Shell String -> CreateProcess
 toProc (Shell a) = shell a
@@ -34,18 +41,40 @@ showHome = Shell "ls"
 hostname = Shell "hostname"
 
 install :: String -> Shell String
-install package = Shell ("sudo apt-get install -y "++package)
+install package = Shell ("sudo DEBIAN_FRONTEND=noninteractive apt-get install -y "++package)
 
 delete :: String -> Shell String
-delete package = Shell ("sudo apt-get remove -y "++package)
+delete package = Shell ("sudo DEBIAN_FRONTEND=noninteractive apt-get remove -y "++package)
 
 ssh :: String -> String -> Shell String
-ssh user host = Shell ("ssh "++user++"@"++host)
+ssh user host = Shell ("ssh "++user++"@"++host++" -c")
+
+sshSh :: String -> String -> Shell String -> Shell String
+sshSh user host (Shell cmd) = Shell ("ssh "++user++"@"++host++" '"++cmd++"'")
+
+commitAndPushGit :: String -> Shell String
+commitAndPushGit message = Shell ("git commit -m \""++message++"\" ; git push")
+
+updateGit :: Shell String
+updateGit = Shell ("cd "++appdir++"; git pull")
+
+cloneGit :: Shell String
+cloneGit = Shell ("git clone https://github.com/lakowske/hawt-spot "++appdir)
+
+cleanGit :: Shell String
+cleanGit = Shell ("rm -rf "++appdir)
+
+iptablesNat :: Shell String
+iptablesNat = Shell ("sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE")
+
+saveTables :: Shell String
+saveTables = Shell ("sudo netfilter-persistent save")
 
 shconcat :: Shell String -> Shell String -> Shell String
 shconcat (Shell a) (Shell b) = Shell (a++" "++b)
 
-
+remoteEx :: Shell String -> IO String
+remoteEx cmd = readShell $ sshSh globaluser globalhost cmd
 
 main = getArgs >>= parse >>= putStr . tac
 
