4ad97705f4f4cc1a5e19826ddeadbfe1ec46b0b1
Seth Lakowske <lakowske@gmail.com>
2020-12-05 00:22:31 -0500

    update dnsmasq hostapd and forward ip

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,8 @@ case "$1" in
   install)                                                                                                                                                                                                                            
     sudo apt-get update                                                                                                                                                                                                             
     sudo apt-get upgrade -y                                                                                                                                                                                                         
-    sudo apt-get install -y hostapd dnsmasq                                                                                                                                                                                         
+    sudo apt-get install -y hostapd dnsmasq iptables iptables-persistent bridge-utils
+
     ;;                                                                                                                                                                                                                              
   delete)                                                                                                                                                                                                                             
     sudo apt-get remove -y                                                                                                                                                                                                          
--- scripts/op.sh
+++ scripts/op.sh
@@ -1,16 +1,24 @@
+set -ex
+
 case "$1" in                                                                                                                                                                                                                            
-  install)                                                                                                                                                                                                                            
-    sudo apt-get update                                                                                                                                                                                                             
-    sudo apt-get upgrade -y                                                                                                                                                                                                         
-    sudo apt-get install -y hostapd dnsmasq
-    sudo systemctl stop hostapd
-    sudo systemctl stop dnsmasq    
-    sudo cp dnsmasq.conf /etc
-    sudo cp                                                                                                                                                                                          
-    ;;                                                                                                                                                                                                                              
-  delete)                                                                                                                                                                                                                             
-    sudo apt-get remove -y                                                                                                                                                                                                          
-    hostapd dnsmasq                                                                                                                                                                                                                 
-    rm -rf $APPDIR                                                                                                                                                                                                                  
-    ;;                                                                                                                                                                                                                              
+    install)
+	sudo bash scripts/install.sh install
+	sudo cp dnsmasq.conf /etc
+	sudo cp interfaces /etc/network
+	sudo cp hostapd.conf /etc/hostapd
+	sudo cp hostapd /etc/default
+	sudo cp sysctl.conf /etc
+	sudo systemctl stop dnsmasq
+	sudo systemctl unmask hostapd
+	sudo systemctl enable hostapd
+	sudo iptables -t nat -A POSTROUTING -j MASQUERADE
+	sudo iptables -A FORWARD -i eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+	sudo iptables -A FORWARD -i wlan0 -j ACCEPT
+	sudo iptables-save > /etc/iptables/rules.v4
+	;;
+
+    delete)
+	sudo apt-get remove -y hostapd dnsmasq
+	#rm -rf $APPDIR
+	;;
 esac  
